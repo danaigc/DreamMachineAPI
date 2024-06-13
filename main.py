70a4854866fd717e2ec801bbaade13f9bcd395d9
danaigc <167301973+danaigc@users.noreply.github.com>
2024-06-13 13:21:16 +0800

    update

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from util import dreamMachineMake, refreshDreamMachine
 
 
 async def main():
+    # Your access_token
     access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcl91dWlkIjoiYWE2MzVkY2EtMWU0NC00YjFiLTk1YTItOWEyYjA1ZWY3NTAxIiwiY2xpZW50X2lkIjoiIn0sImV4cCI6MTcxODg1Mjk4MX0.QdmkCSJ0NTvU-l2_vfhuNAAiYJpNPfurXcC6R36435A"
-    # 这里替换成您的access_token
     prompt = "Little pigs are running on the grass"
     dreamMachineMake(prompt, access_token)
 
