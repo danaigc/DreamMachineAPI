5b871296b5a3ab350fb14a4e8f1daa3709953841
danaigc <167301973+danaigc@users.noreply.github.com>
2024-06-13 12:10:24 +0800

    update

---
+++ main.py
@@ -0,0 +1,29 @@
+import asyncio
+
+import aiohttp
+
+from util import dreamMachineMake, refreshDreamMachine
+
+
+async def main():
+    access_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOnsidXNlcl91dWlkIjoiYWE2MzVkY2EtMWU0NC00YjFiLTk1YTItOWEyYjA1ZWY3NTAxIiwiY2xpZW50X2lkIjoiIn0sImV4cCI6MTcxODg1Mjk4MX0.QdmkCSJ0NTvU-l2_vfhuNAAiYJpNPfurXcC6R36435A"
+    # 这里替换成您的access_token
+    prompt = "Little pigs are running on the grass"
+    dreamMachineMake(prompt, access_token)
+
+    async with aiohttp.ClientSession() as session:
+        previous_ids = set()
+        while True:
+            response_json = await refreshDreamMachine(session, access_token)
+            item = response_json[0]
+            if item['id'] not in previous_ids and item['state'] == 'completed':
+                previous_ids.add(item['id'])
+                if item['video']:
+                    print(f"New video link: {item['video']['url']}")
+                    break
+            await asyncio.sleep(3)
+
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ util.py
@@ -0,0 +1,38 @@
+import json
+
+import requests
+
+
+def dreamMachineMake(prompt, access_token):
+    url = "https://internal-api.virginia.labs.lumalabs.ai/api/photon/v1/generations/"
+
+    payload = {
+        "user_prompt": prompt,
+        "aspect_ratio": "16:9",
+        "expand_prompt": True
+    }
+    headers = {
+        "Cookie": "access_token=" + access_token,
+        "Origin": "https://lumalabs.ai",
+        "Referer": "https://lumalabs.ai",
+        "content-type": "application/json"
+    }
+
+    response = requests.post(url, json=payload, headers=headers)
+    print(response.text)
+
+
+# 定义刷新获取结果的异步函数
+async def refreshDreamMachine(session, access_token):
+    url = "https://internal-api.virginia.labs.lumalabs.ai/api/photon/v1/user/generations/"
+    querystring = {"offset": "0", "limit": "10"}
+    headers = {
+        "Cookie": "access_token=" + access_token,
+    }
+
+    async with session.get(url, headers=headers, params=querystring) as response:
+        response_text = await response.text()
+        response_json = json.loads(response_text)
+        return response_json
+
+
